67b6ee0c5f0ed7f7d05d62af2fa2df5611e7db15
reflective-mapping-9478 <ejrobe@gmail.com>
2018-11-28 08:34:12 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
+def old_square_array(array)
+  squared_array = []
+  for each in array do
+    squared = each * each
+    squared_array << squared
+  end
+  return squared_array
+end
+
 def square_array(array)
-  # your code here
+  array.each do
 end
